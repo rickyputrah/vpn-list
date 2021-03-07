431baad08376d23ab3c8b7b5ef9dd8bb3da911d2
rickyputrah <ricky@ula.app>
2021-03-07 13:02:32 +0800

    Clean up Unused Code in ApiServiceImpl

--- app/src/main/java/com/rickyputrah/express/network/ApiServiceImpl.kt
+++ app/src/main/java/com/rickyputrah/express/network/ApiServiceImpl.kt
@@ -7,14 +7,8 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory
-import java.security.cert.CertificateException
-import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
 import javax.inject.Inject
-import javax.net.ssl.HostnameVerifier
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManager
-import javax.net.ssl.X509TrustManager
 
 
 class ApiServiceImpl @Inject constructor(private val context: Context) : ApiService {
@@ -23,7 +17,6 @@ class ApiServiceImpl @Inject constructor(private val context: Context) : ApiServ
         val httpClientBuilder = OkHttpClient.Builder()
             .apply {
                 applyLoggingInterceptor(this)
-                applyDebugCertificate(this)
                 connectTimeout(CONNECT_TIMEOUT, TimeUnit.SECONDS)
             }
 
@@ -46,47 +39,6 @@ class ApiServiceImpl @Inject constructor(private val context: Context) : ApiServ
         val loggingInterceptor = HttpLoggingInterceptor().apply {
             level = HttpLoggingInterceptor.Level.BODY
         }
-
         builder.addInterceptor(loggingInterceptor)
-    }
-
-    private fun applyDebugCertificate(builder: OkHttpClient.Builder) {
-        try {
-            // Create a trust manager that does not validate certificate chains
-            val sslManager = object : X509TrustManager {
-
-                override fun getAcceptedIssuers(): Array<X509Certificate> {
-                    return arrayOf()
-                }
-
-                @Throws(CertificateException::class)
-                override fun checkClientTrusted(
-                    chain: Array<X509Certificate>,
-                    authType: String
-                ) {
-                }
-
-                @Throws(CertificateException::class)
-                override fun checkServerTrusted(
-                    chain: Array<X509Certificate>,
-                    authType: String
-                ) {
-                }
-            }
-
-            val trustAllCerts = arrayOf<TrustManager>(sslManager)
-
-            // Install the all-trusting trust manager
-            val sslContext = SSLContext.getInstance("SSL")
-            sslContext.init(null, trustAllCerts, java.security.SecureRandom())
-
-            // Create an ssl socket factory with our all-trusting manager
-            val sslSocketFactory = sslContext.socketFactory
-
-            builder.sslSocketFactory(sslSocketFactory, sslManager)
-            builder.hostnameVerifier(HostnameVerifier { _, _ -> true })
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
     }
 }
